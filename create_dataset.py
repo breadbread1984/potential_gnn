f400e6f20f8040b5a40e19f6b69f21c45dfee590
breadbread1984 <breadbread1984@163.com>
2024-09-26 10:59:16 +0800

    update

--- create_dataset.py
+++ create_dataset.py
@@ -8,7 +8,10 @@ from bisect import bisect
 import mysql.connector
 import numpy as np
 from bisect import bisect
+import torch
 from torch.utils.data import Dataset
+from torch_geometric.data impmort Data
+import faiss
 
 FLAGS = flags.FLAGS
 
@@ -48,29 +51,41 @@ def main(unused_argv):
   np.save(join(FLAGS.output, '%s_%f.npy' % (FLAGS.smiles, FLAGS.bond_dist)), output)
 
 class RhoDataset(Dataset):
-  def __init__(self, dir_path):
-    self.npys = list()
-    for root, dirs, files in walk(dir_path):
-      for f in files:
-        stem, ext = splitext(f)
-        if ext != '.npy': continue
-        self.npys.append(np.load(join(root, f), mmap_mode = 'r'))
-    self.start_indices = [0] * len(self.npys)
-    self.data_count = 0
-    for index, memmap in enumerate(self.npys):
-      self.start_indices[index] = self.data_count
-      self.data_count += memmap.shape[0]
+  def __init__(self, dataset_dir, k = 4):
+    self.k = k
+    res = faiss.StandardGpuResources()
+    flat_config = faiss.GpuIndexFlatConfig()
+    flat_config.device = 0
+    self.index = faiss.GpuIndexFlatL2(res, 3, flat_config)
+    dataset = list()
+    for f in listdir(dataset_dir):
+      dataset.append(np.load(join(dataset_dir, f)))
+    dataset = np.concatenate(dataset, axis = 0)
+    self.rho = np.ascontiguousarray(dataset[:,:739].astype(np.float32)) # rho.shape = (num, 739)
+    self.pos = np.ascontiguousarray(dataset[:,769:769+3].astype(np.float32)) # pos.shape = (num, 3)
+    self.exc = np.ascontiguousarray(dataset[:,769+3].astype(np.float32)) # exc.shape = (num)
+    self.vxc = np.ascontiguousarray(dataset[:,769+4].astype(np.float32)) # vxc.shape = (num)
+    self.index.add(self.pos)
   def __len__(self):
-    return self.data_count
+    return len(self.rho)
   def __getitem__(self, index):
-    memmap_index = bisect(self.start_indices, index) - 1
-    index_in_memmap = index - self.start_indices[memmap_index]
-    data = self.npys[memmap_index][index_in_memmap]
-    rho = data[:739].astype(np.float32)
-    pos = data[769:769 + 3]
-    exc = data[769 + 3].astype(np.float32)
-    vxc = data[769 + 4].astype(np.float32)
-    return rho, pos, exc, vxc
+    pos = self.pos[index:index+1] # pos.shape = (1,3)
+    D, I = self.index.search(pos, self.k) # D.shape = (1, k) I.shape = (1, K)
+    neighbor_rho = np.squeeze(self.rho[I,:], axis = 0) # neighbor_rho.shape = (K, 739)
+    neighbor_pos = np.squeeze(self.pos[I,:], axis = 0) # neighbor_pos.shape = (K, 3)
+    rho = np.expand_dims(self.rho[index,:], axis = 0) # rho.shape = (1, 739)
+    pos = np.expand_dims(self.pos[index,:], axis = 0) # pos.shape = (1, 3)
+    x = np.concatenate([rho, neighbor_rho], axis = 0) # rhos.shape = (K+1, 739)
+    x_pos = np.concatenate([pos, neighbor_pos], axis = 0) # pos.shape = (K+1, 3)
+    edge_index = list()
+    for i in range(1, self.k + 1):
+      edge_index.append([i,0])
+      edge_index.append([0,i])
+    edge_index = torch.tensor(edge_index, dtype = torch.long).t().contiguous() # edge_index.shape = (2, edge_num)
+    exc = self.exc[index] # exc.shape = ()
+    vxc = self.vxc[index] # vxc.shape = ()
+    data = Data(x = torch.from_numpy(x), x_pos = torch.from_numpy(x_pos), edge_index = edge_index, exc = torch.tensor(exc, dtype = torch.float32), vxc = torch.tensor(vxc, dtype = torch.float32))
+    return data
 
 if __name__ == "__main__":
   add_options()
