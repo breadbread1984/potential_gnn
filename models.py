0de946610523c7e2a09b642c2c86578267bc9d53
breadbread1984 <breadbread1984@163.com>
2024-09-27 07:03:12 +0000

    updat

--- models.py
+++ models.py
@@ -1,21 +1,61 @@
 #!/usr/bin/python3
 
+from collections import OrderedDict
 import torch
 from torch import nn
-from torch_geometric.nn import global_mean_pool, GATv2Conv
+import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing, global_mean_pool, aggr
+
+class AeroConv(MessagPassing):
+  def __init__(self, channels = 256, drop_rate = 0.2):
+    super(AeroConv, self).__init__(aggr = None)
+    pass
+  def forward(self, x, edge_index):
+    pass
+
+class InitFeat(nn.Module):
+  def __init__(self, in_channels = 739, hid_channels = 64, dense_layer_num = 2, drop_rate = 0.5):
+    super(InitFeat, self).__init__()
+    init_feat_layers = OrderedDict()
+    for i in range(dense_layer_num):
+      if i != 0: init_feat_layers[f'elu_{i}'] = nn.ELU()
+      init_feat_layers[f'drop_{i}'] = nn.Dropout(drop_rate)
+      init_feat_layers[f'dense_{i}'] = nn.Linear(hid_channels if i != 0 else in_channels, hid_channels)
+    self.init_feat = nn.Sequential(init_feat_layers)
+  def forward(self, x):
+    return self.init_feat(x)
+
+class UpdateZ(nn.Module):
+  def __init__(self, k, channels = 64, head = 1, lambd = 1):
+    super(UpdateZ, self).__init__()
+    self.hop_att = nn.Parameter(torch.empty(1, head, (channels // head) if k == 0 else (channels // head * 2)), requires_grad = True) # hop_att.shape = (1, head, channels // head or channels // head * 2)
+    self.hop_bias = nn.Parameter(torch.empty(1, head), requires_grad = True) # hop_bias.shape = (1, head)
+    self.k = k
+    self.channels = channels
+    self.lambd = lambd
+  def forward(self, h, z):
+    h = torch.reshape(h, (-1, self.head, self.channels // self.head)) # h.shape = (node_num, head, channels // head)
+    if self.k != 0:
+      z_scale = z * torch.log((self.lambd / self.k) + (1 + 1e-6)) # z.shape = (node_num, head, channels // head)
+      g = torch.cat([h, z_scale], dim = -1) # g.shape = (node_num, head, channels // head * 2)
+    else:
+      g = h # g.shape = (node_num, head, channel // head)
+    hop_attention = F.elu(g) # hop_attention.shape = (node_num, head, channel // head or channels // head * 2)
+    hop_attention = torch.sum(self.hop_att * hop_attention, dim = -1) + self.hop_bias # hop_attention.shape = (node_num, head)
+    hop_attention = torch.unsqueeze(hop_attention, dim = -1) # hop_attention.shape = (node_num, head, 1)
+    if self.k == 0:
+      z = h * hop_attention # z.shape = (node_num, head, channel // head)
+    else:
+      z = z + h * hop_attention # z.shape = (node_num, head, channel // head)
+    return z
 
 class PotentialPredictor(nn.Module):
-  def __init__(self, channels = 256, layer_num = 4, drop_rate = 0.2):
+  def __init__(self, in_channels = 739, hid_channels = 64, dense_layer_num = 2, drop_rate = 0.5):
     super(PotentialPredictor, self).__init__()
-    self.dense = nn.Linear(739, channels)
-    self.convs = nn.ModuleList([GATv2Conv(channels, channels, 8, dropout = drop_rate) for _ in range(layer_num)])
-    self.head = nn.Linear(channels, 1)
+    self.init_feat = InitFeat(in_channels, hid_channels, dense_layer_num, drop_rate)
   def forward(self, data):
     x, edge_index, batch = data.x, data.edge_index, data.batch
-    results = self.dense(x)
-    for conv in self.convs:
-      results = conv(results, edge_index)
-    results = global_mean_pool(results, batch)
-    results = self.head(results)
+    results = self.init_feat(x) # results.shape = (node_num, hid_channels)
+
     return results
 
