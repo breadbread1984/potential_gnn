4c0516c0a76ef6be14f1e0dd6e1b9e73b134c5ef
breadbread1984 <breadbread1984@163.com>
2024-09-27 00:48:56 +0000

    update

--- create_dataset.py
+++ create_dataset.py
@@ -56,7 +56,7 @@ class RhoDataset(Dataset):
     res = faiss.StandardGpuResources()
     flat_config = faiss.GpuIndexFlatConfig()
     flat_config.device = 0
-    self.index = faiss.GpuIndexFlatL2(res, 3, flat_config)
+    self.index = faiss.GpuIndexFlatL2(res, 739, flat_config)
     dataset = list()
     for f in listdir(dataset_dir):
       dataset.append(np.load(join(dataset_dir, f)))
@@ -65,12 +65,12 @@ class RhoDataset(Dataset):
     self.pos = np.ascontiguousarray(dataset[:,769:769+3].astype(np.float32)) # pos.shape = (num, 3)
     self.exc = np.ascontiguousarray(dataset[:,769+3].astype(np.float32)) # exc.shape = (num)
     self.vxc = np.ascontiguousarray(dataset[:,769+4].astype(np.float32)) # vxc.shape = (num)
-    self.index.add(self.pos)
+    self.index.add(self.rho)
   def __len__(self):
     return len(self.rho)
   def __getitem__(self, index):
-    pos = self.pos[index:index+1] # pos.shape = (1,3)
-    D, I = self.index.search(pos, self.k) # D.shape = (1, k) I.shape = (1, K)
+    rho = self.rho[index:index+1] # pos.shape = (1,3)
+    D, I = self.index.search(rho, self.k) # D.shape = (1, k) I.shape = (1, K)
     neighbor_rho = np.squeeze(self.rho[I,:], axis = 0) # neighbor_rho.shape = (K, 739)
     neighbor_pos = np.squeeze(self.pos[I,:], axis = 0) # neighbor_pos.shape = (K, 3)
     rho = np.expand_dims(self.rho[index,:], axis = 0) # rho.shape = (1, 739)
