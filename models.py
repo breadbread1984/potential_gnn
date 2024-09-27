9a309b94dfb3c5d14f4b5b3bef041045f2445b9f
breadbread1984 <breadbread1984@163.com>
2024-09-27 06:14:32 +0000

    update

--- models.py
+++ models.py
@@ -2,13 +2,13 @@
 
 import torch
 from torch import nn
-from torch_geometric.nn import global_mean_pool, GATConv
+from torch_geometric.nn import global_mean_pool, GATv2Conv
 
 class PotentialPredictor(nn.Module):
   def __init__(self, channels = 256, layer_num = 4, drop_rate = 0.2):
     super(PotentialPredictor, self).__init__()
     self.dense = nn.Linear(739, channels)
-    self.convs = nn.ModuleList([GATConv(channels, channels, 8, dropout = drop_rate) for _ in range(layer_num)])
+    self.convs = nn.ModuleList([GATv2Conv(channels, channels, 8, dropout = drop_rate) for _ in range(layer_num)])
     self.head = nn.Linear(channels, 1)
   def forward(self, data):
     x, edge_index, batch = data.x, data.edge_index, data.batch
